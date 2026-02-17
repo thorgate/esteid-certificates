87e067d749d80d38e98355ffcf3f28ec7d1b8209
Sergey Kovalskiy <sergey@thorgate.eu>
2026-02-17 17:57:30 +0200

    chore: update SK certificates to include new timestamping unit

--- esteid_certificates/__init__.py
+++ esteid_certificates/__init__.py
@@ -4,7 +4,7 @@ import warnings
 from .constants import ISSUER_CERTS, ROOT_CA_FILES, TEST_ROOT_CA_FILES
 
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 
 class UnknownCertificateError(Exception):
